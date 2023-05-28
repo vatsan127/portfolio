61ced4913165dc3c1d203e396e0524a5b1b2d282
vatsan127 <110617159+vatsan127@users.noreply.github.com>
2023-05-28 12:46:27 +0530

    bg color added for all profiles

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const profiles = [
     name: "Naruto",
     level: "Genin",
     des: "Naruto Uzumaki is a spirited ninja from the Hidden Leaf Village, determined to become the Hokage, while facing challenges, forming bonds, and mastering powerful techniques on his journey.",
+    bgColor: "orange",
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ const profiles = [
     name: "Sasuke ",
     level: "Genin",
     des: "Sasuke Uchiha, a complex character from the Naruto series, is driven by revenge and seeks power to avenge his clan. He undergoes personal growth, battles inner demons, and ultimately finds redemption.",
+    bgColor: "pruple",
   },
   {
     id: 3,
@@ -27,6 +29,7 @@ const profiles = [
     name: "Sakura",
     level: "Jonin",
     des: "Sakura Haruno is a ninja from the Naruto series. Initially weak, she trains diligently and becomes a strong kunoichi, known for her intelligence, medical abilities, and unwavering loyalty to her friends.",
+    bgColor: "pink",
   },
   {
     id: 4,
@@ -34,6 +37,7 @@ const profiles = [
     name: "Kakashi",
     level: "Jonin",
     des: "Kakashi Hatake, a skilled shinobi from the Naruto series, is known for his mysterious nature, Sharingan eye, excellence in combat, and being a mentor to Team 7.",
+    bgColor: "#66f4d3",
   },
   {
     id: 5,
@@ -41,10 +45,26 @@ const profiles = [
     name: "Jiraya",
     level: "Sanin",
     des: "Jiraiya, a legendary shinobi from the Hidden-Leaf Village, was a wise and powerful teacher, known for his eccentricity, mastery of the toad-based fighting style, and his impact on Naruto's growth.",
+    bgColor: "red",
   },
 ];
+
+const btnNext = document.getElementById("btn-next");
+const btnPrev = document.getElementById("btn-prev");
 
 const profileImg = document.getElementById("profile-img");
 const profileName = document.getElementById("profile-name");
 const profileLevel = document.getElementById("profile-level");
 const prpofileDes = document.getElementById("profile-des");
+
+console.log("profiles length ", profiles.length);
+btnNext.addEventListener("click", function () {
+  let counter = 0;
+  if (counter < profiles.length - 1) {
+    counter++;
+  } else {
+    counter = 0;
+  }
+  console.log("counter ", counter);
+  profileImg.img = profileImg;
+});
